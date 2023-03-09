d642fe25c11aa03d78c9596c0e85a90d03fe0820
fantadrinker <yizhi.tsui@gmail.com>
2023-03-08 20:27:02 -0800

    fix warnings

--- frontend/src/Components/NavBar.js
+++ frontend/src/Components/NavBar.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { NavLink as RouterNavLink, Link } from "react-router-dom";
+import React from "react";
+import { Link } from "react-router-dom";
 
 import { useAuth0 } from "@auth0/auth0-react";
 import LoginButton from "./LoginAuth0";
