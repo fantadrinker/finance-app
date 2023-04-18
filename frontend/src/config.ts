c227a82e24739237ad6f8eec78411a5945b73d76
fantadrinker <yizhi.tsui@gmail.com>
2023-04-18 07:40:00 -0700

    more migrations

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -13,7 +13,7 @@ export function getConfig(): AuthConfig {
   // don't have an API).
   // If this resolves to `null`, the API page changes to show some helpful info about what to do
   // with the audience.
-  const audience =
+  const audience: string =
     configJson.audience && configJson.audience !== "YOUR_API_IDENTIFIER"
       ? configJson.audience
       : null;
