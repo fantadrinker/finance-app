7b80c306be499cd4118c017d57b49ab36196fa14
fantadrinker <yizhi.tsui@gmail.com>
2023-09-19 21:05:15 -0700

    wishlist form attributes

--- frontend/src/Pages/Wishlist/Wishlist.tsx
+++ frontend/src/Pages/Wishlist/Wishlist.tsx
@@ -35,20 +35,20 @@ export function Wishlist() {
 
     <Form className="flex flex-row" >
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Enter name" value={newName} onChange={e => setNewName(e.target.value)} />
+        <Form.Label for="item-name">Name</Form.Label>
+        <Form.Control id="item-name" type="text" placeholder="Enter name" value={newName} onChange={e => setNewName(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Description</Form.Label>
-        <Form.Control type="text" placeholder="Enter description" value={newDescription} onChange={e => setNewDescription(e.target.value)} />
+        <Form.Label for="item-description" >Description</Form.Label>
+        <Form.Control id="item-description" type="text" placeholder="Enter description" value={newDescription} onChange={e => setNewDescription(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>URL</Form.Label>
-        <Form.Control type="text" placeholder="Enter URL" value={newUrl} onChange={e => setNewUrl(e.target.value)} />
+        <Form.Label for="item-url" >URL</Form.Label>
+        <Form.Control id="item-url" type="text" placeholder="Enter URL" value={newUrl} onChange={e => setNewUrl(e.target.value)} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Price</Form.Label>
-        <Form.Control type="text" placeholder="Enter price" value={newPrice} onChange={e => setNewPrice(parseInt(e.target.value))} />
+        <Form.Label for="item-price" >Price</Form.Label>
+        <Form.Control id="item-price" type="number" placeholder="Enter price" value={newPrice} onChange={e => setNewPrice(parseInt(e.target.value))} />
       </Form.Group>
       <button type="submit" className="btn btn-primary" onClick={addNewItem}>Submit</button>
     </Form>
