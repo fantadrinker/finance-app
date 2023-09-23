859d190b19a1a01d6a58dee1a4b14d5a290ceb8b
fantadrinker <yizhi.tsui@gmail.com>
2023-09-22 23:11:53 -0700

    fix method

--- backend_lambdas_sam/lambdas/wishlist/app.py
+++ backend_lambdas_sam/lambdas/wishlist/app.py
@@ -1,5 +1,28 @@
+import json
 
-
+# mock data here:
+mocks = [
+    {
+        "user": "test",
+        "sk": "wishlist#12324234234",
+        "name": "graphics card",
+        "description": "RTX 3080",
+        "category": "Grocery",
+        "url": "https://https://www.newegg.ca/msi-geforce-rtx-4070-rtx-4070-ventus-2x-12g-oc/p/N82E16814137787?item=N82E16814137787",
+        "price": 1000,
+        "priority": 1,
+    },
+    {
+        "user": "test",
+        "sk": "wishlist#12324234235",
+        "name": "desk lamp",
+        "description": "desk lamp",
+        "category": "Furniture",
+        "url": "https://www.amazon.ca/ACNCTOP-Desk-Lamp-Office-Home/dp/B0BGNJS7FQ/ref=sr_1_7?crid=W3YGOGFHLWPQ&keywords=desk+lamp&qid=1695447981&sprefix=desk+lamp%2Caps%2C360&sr=8-7",
+        "price": 50,
+        "priority": 2,
+    }
+]
 
 
 def get(user_id):
@@ -7,7 +30,9 @@ def get(user_id):
     print('get wishlist', user_id)
     return {
         "statusCode": 200,
-        "body": "success get"
+        "body": json.dumps({
+            "data": mocks,
+        })
     }
 
 def post(user_id, event):
@@ -53,13 +78,15 @@ def lambda_handler(event, context):
     #    table_name = os.environ.get("MAPPINGS_TABLE", "")
     #    table = dynamodb.Table(table_name)
     user_id = "test"
-    if event["httpMethod"] == "GET":
+    print(event)
+    method = event.get("requestContext", {}).get("http", {}).get("method", "")
+    if method == "GET":
         return get(user_id)
-    elif event["httpMethod"] == "POST":
+    elif method == "POST":
         return post(user_id, event)
-    elif event["httpMethod"] == "DELETE":
+    elif method == "DELETE":
         return delete(user_id, event)
-    elif event["httpMethod"] == "PUT":
+    elif method == "PUT":
         return put(user_id, event)
     else:
         return {
