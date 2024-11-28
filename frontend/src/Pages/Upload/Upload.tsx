9dc2cd434f98f683c6a6ca8f3694d642f5a49f20
fantadrinker <yizhi.tsui@gmail.com>
2024-11-27 22:33:09 -0800

    fix build

--- frontend/src/Pages/Upload/Upload.tsx
+++ frontend/src/Pages/Upload/Upload.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, ChangeEvent, useMemo, useCallback, useContext } from 'react'
+import React, { useState, useEffect, ChangeEvent, useMemo, useCallback } from 'react'
 import md5 from 'md5'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
@@ -8,7 +8,7 @@ import { ActivityRow, FileUpload, getUploads, previewActivities } from '../../ap
 import { useAuth0TokenSilent } from '../../hooks'
 import { ButtonGroup } from 'react-bootstrap'
 import { UploadPreviewModal } from '../../Components/UploadPreviewModal'
-import { MultiSelectContext, MultiSelectContextProviderWrapper } from '../../Contexts/MultiSelectContext'
+import {  MultiSelectContextProviderWrapper } from '../../Contexts/MultiSelectContext'
 
 enum ColumnFormat {
   cap1 = 'cap1',
